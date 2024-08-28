2ead267e2070d440171fdb16c143bbe72f4e7157
Chad <23-04087@g.batstate-u.edu.ph>
2024-08-28 12:35:02 +0800

    Add files via upload
    August 28, 2024

---
+++ Exercise Set 02/bitwise1.py
@@ -0,0 +1,19 @@
+# Bitwise (|)
+result_a = 12 | 25
+print(f"12 | 25 = {result_a}")
+
+# Bitwise (&)
+result_b = 21 & 27
+print(f"21 & 27 = {result_b}")
+
+# Bitwise (^)
+result_c = 8 ^ 7
+print(f" 8 ^ 7 = {result_c}")
+
+# Bitwise (&)
+result_d = 13 & 7
+print(f" 13 & 7 = {result_d}")
+
+# Bitwise (|)
+result_e = 13 | 7
+print(f" 13 | 7 = {result_e}")
